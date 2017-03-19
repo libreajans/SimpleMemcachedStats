3b4dee3e520fa368885219ea4bd2f35fd8896a7a
sabri unal <yakushabb@gmail.com>
2017-03-19 04:24:57 +0200

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,9 +1,19 @@
 <?php
 
 	/**
-	* Forked: https://github.com/bainternet/Memchaced-Dashboard
-	*
-	* Simple memchached dashboard
+	* ENGLISH:
+	* Simple Stats page for Memcached server
+	* Forked from (https://github.com/bainternet/Memchaced-Dashboard)
+	* Turkish language support added, number display divided by thousands separator
+	* Data usage values are converted to readable values from byte values
+	* 
+	* TÜRKÇE:
+	* Memcached server için basit bir istatistik sayfası
+	* Şu projeden türetilmiştir (https://github.com/bainternet/Memchaced-Dashboard)
+	* Türkçe dil desteği eklenmiş, sayı gösterimleri binlik seperatör ile ayrılmış
+	* Veri kullanım değerleri byte değerlerden okunabilir değerlere dönüştürülmüştür
+	
+	* Simple Memchached Dashboard
 	* @version 0.2.0
 	* @author Sabri Ünal <yakushabb@gmail.com>
 	*/
@@ -25,7 +35,7 @@
 	{
 
 		/**
-		| fonksiyon kaynağı: https://github.com/kn007/memcache.php/blob/master/memcache.php
+		* fonksiyon kaynağı: https://github.com/kn007/memcache.php/blob/master/memcache.php
 		*/
 
 		//global yerine biz atalayım
